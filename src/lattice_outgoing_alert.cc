1704b2ae70d471e28910f854522d2c57aac45eb6
Marco <marco.jendryczko@gmx.net>
2017-02-07 18:50:09 +0100

    removed TODOS, added commented out tests because they dont compile atm

--- src/lattice_outgoing_alert.cc
+++ src/lattice_outgoing_alert.cc
@@ -3,7 +3,6 @@
 namespace beemaster {
 
     const broker::message beemaster::LatticeOutgoingAlert::ToMessage() {
-        //TODO: incidents is a vector of incidents, how to handle
         auto ts = std::chrono::duration_cast<std::chrono::duration<double>>(timestamp.time_since_epoch()).count();
         auto msg = broker::message{"Beemaster::lattice_result", broker::time_point(ts), incident};
         //TODO: use if viable again
